c98d57b8011a8eb11d8d38dc51382e30e548d01f
Sky Sayson <Skysayson74@gmail.com>
2023-11-07 23:59:41 +0800

    Made routing changes

--- BookFlow-React/src/main.jsx
+++ BookFlow-React/src/main.jsx
@@ -4,13 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LogIn from './Pages/Log-in.jsx'
-import userStaff from './Pages/Sign-up/UserStaff.jsx'
+import signUpUser from './Pages/Sign-up/UserFields.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div>
     <Router>
       <Route path='/BookFlow-Login' component={LogIn}></Route>
-      <Route path='/BookFlow-UserStaff' component={userStaff}></Route>
+      <Route path='/BookFlow-Signup' component={signUpUser}></Route>
     </Router>
   </div>
 )
