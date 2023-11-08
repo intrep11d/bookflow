1e939c2d94a962a59c785dcd4ac8f983ab4c51ce
unknown <jonazjuan.dev@gmail.com>
2023-11-08 08:58:59 +0800

    Completed userinfo form

--- BookFlow-React/src/Pages/Sign-up/UserFields.jsx
+++ BookFlow-React/src/Pages/Sign-up/UserFields.jsx
@@ -1,13 +1,15 @@
 function signUpUser() {
   return (
     <div className="Signup-main-div flex h-screen w-screen justify-evenly items-center flex-col bg-black">
-        <div className="flex text-white p-[2rem]">
-            NAVBAR
-        </div>
-        <div
-          className="bgPic absolute h-screen w-screen bg-[url('./assets/book-bg.jpg')] bg-cover bg-center opacity-10"
-          style={{ pointerEvents: "none" }}
-        />
+      <div className="navBar-div flex w-[100%] justify-between">
+        <div className="flex text-white pl-[3rem] pt-[1rem]">Logo</div>
+        <div className="flex text-white pr-[3rem] pt-[1rem]">Test</div>
+      </div>
+
+      <div
+        className="bgPic absolute h-screen w-screen bg-[url('./assets/book-bg.jpg')] bg-cover bg-center opacity-10"
+        style={{ pointerEvents: "none" }}
+      />
       <div className="Signup-wrapper-div flex h-screen w-screen justify-evenly items-center">
         <form method="POST" action="" className="inputFields-div flex flex-col">
           <div className="name-div flex mb-[2rem]">
@@ -40,13 +42,29 @@ function signUpUser() {
               type="text"
             />
 
-            <button className="flex border  border-[#D5C5AE] transition-[0.1s] hover:text-black hover:bg-[#D5C5AE] text-[#D5C5AE] justify-center rounded-lg  p-[0.5rem] w-[20.5rem]">
-              Submit
+            <button className="flex border mt-[3rem] border-[#D5C5AE] transition-[0.1s] hover:text-black hover:bg-[#D5C5AE] text-[#D5C5AE] justify-center rounded-lg  p-[0.5rem] w-[20.5rem]">
+              Next
             </button>
           </div>
         </form>
 
-        <div className="divPic flex  text-white">PIC GOES HERE</div>
+        <div className="divPic flex  text-white">
+          <svg
+            width="20rem"
+            height="20rem"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+              stroke="#D5C5AE"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   );
