3c8ae660333c9786676ef26d226173e992ba8420
unknown <jonazjuan.dev@gmail.com>
2023-11-08 08:59:30 +0800

    Added routing to password form signup

--- BookFlow-React/src/main.jsx
+++ BookFlow-React/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LogIn from './Pages/Log-in.jsx'
+import passwordField from './Pages/Sign-up/UserField-pw.jsx'
 import signUpUser from './Pages/Sign-up/UserFields.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <Route path='/BookFlow-Login' component={LogIn}></Route>
       <Route path='/BookFlow-Signup' component={signUpUser}></Route>
+      <Route path='/BookFlow-Password' component={passwordField}></Route>
     </Router>
   </div>
 )
