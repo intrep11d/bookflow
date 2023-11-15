5c76b77735f85992b1f08f75d1e9bcb84fe8b14a
Sky Sayson <Skysayson74@gmail.com>
2023-11-15 17:39:12 +0800

    Carousel in the making

--- BookFlow-React/src/Components/Book-Cards.jsx
+++ BookFlow-React/src/Components/Book-Cards.jsx
@@ -1,11 +1,14 @@
 import atomichabitsImage from "./atomic-habits.jpg";
+import { useState } from "react";
 
 function BookCards() {
+  const [book1, setBook1] = useState("Atomic Habits");
+
   return (
-    <div className="flex w-[20rem] mt-[1%] h-[12.5rem] mr-[1.5rem]">
+    <div className="flex w-[19rem] mt-[1%] h-[12.5rem] mr-[1.5rem] border rounded-xl">
       {/* Use the imported image */}
-      <img src={atomichabitsImage} alt="" className="shadow-xl"/>
-      <p className="flex justify-center w-[100%]">Atomic Habits Blood!</p>
+      <img src={atomichabitsImage} alt="" className="flex ml-[1rem] mt-[1rem] shadow-xl w-[50%] h-[75%]"/>
+      <p className="flex justify-center w-[100%] mt-[0.5rem]">{book1}</p>
     </div>
   );
 }
