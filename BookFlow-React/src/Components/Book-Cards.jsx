011efca89c456cc1cdbba2e9342170814465d195
Sky Sayson <Skysayson74@gmail.com>
2023-11-17 09:42:35 +0800

    Working on Carousel

--- BookFlow-React/src/Components/Book-Cards.jsx
+++ BookFlow-React/src/Components/Book-Cards.jsx
@@ -4,11 +4,43 @@ import { useState } from "react";
 function BookCards() {
   const [book1, setBook1] = useState("Atomic Habits");
 
+  const data = [
+    {
+      title: `Atomic Habits`,
+      img: atomichabitsImage,
+      synopsis: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, tenetur.`,
+    },
+    {
+      title: `Michael Jordan`,
+      img: atomichabitsImage,
+      synopsis: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, officia?`,
+    },
+    {
+      title: `Dave Michael`,
+      img: atomichabitsImage,
+      synopsis: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, exercitationem!`,
+    },
+  ];
+
   return (
-    <div className="flex w-[19rem] mt-[1%] h-[12.5rem] mr-[1.5rem] border rounded-xl">
-      {/* Use the imported image */}
-      <img src={atomichabitsImage} alt="" className="flex ml-[1rem] mt-[1rem] shadow-xl w-[50%] h-[75%]"/>
-      <p className="flex justify-center w-[100%] mt-[0.5rem]">{book1}</p>
+    <div className="flex">
+      {data.map((data) => (
+        <div className="Carousel flex w-[19rem] mt-[1%] h-[12.5rem] mr-[1.5rem] border rounded-xl">
+          <div className="imgParent flex">
+            <div className="imgDiv flex w-[70%]">
+              <img src={data.img} alt="" />
+            </div>
+            <div className="flex flex-col">
+              <div className="synopsisDiv flex w-[100%]">
+                  <h1>{data.title}</h1>
+                </div>
+                <div className="bookTitle flex text-[0.2rem]">
+                <p>{data.synopsis}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
