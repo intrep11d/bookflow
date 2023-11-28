8783bea9e60d5cdbf7a925f32508c9ecfb13ffb9
Sky Sayson <Skysayson74@gmail.com>
2023-11-29 00:50:28 +0800

    userHome is almost done

--- BookFlow-React/src/Components/BookPanel.jsx
+++ BookFlow-React/src/Components/BookPanel.jsx
@@ -5,11 +5,11 @@ import atomBook from "./atomic-habits.jpg";
 function BookPanel(props) {
   const { image, author, title, Synopsis } = props;
   return (
-    <div className="flex BookPanel w-[95%] h-[10rem] bg-[url('/atomic-habits.jpg')] pt-[1rem] pb-[1rem] pl-[1rem] pr-[1rem] shadow-2xl rounded-xl bg-red-400">
+    <div className="flex BookPanel w-[90%] h-[12rem] items-center bg-[url('/atomic-habits.jpg')] pt-[1rem] pb-[1rem] pl-[1rem] pr-[1rem] shadow-2xl rounded-xl bg-red-400">
       <img
         src={atomBook}
         alt=""
-        className=" w-[8rem] h-[7rem] mt-[0.5rem] rounded-xl shadow-[6px_6px_7px_2px_rgba(0,0,0,0.3)]"
+        className=" w-[9rem] h-[8rem] mt-[rem] rounded-xl shadow-[6px_6px_7px_2px_rgba(0,0,0,0.3)]"
       />
       <div className="bookPanelInfo flex flex-col ml-[1rem]">
         <h1 className="text-[1rem]">Atomic Habits</h1>
@@ -18,8 +18,8 @@ function BookPanel(props) {
           <h1 className="ml-[0.3rem]">{props.author} James clear</h1>
         </div>
         <h1 className="text-[0.6rem]">rating</h1>
-        <p className="mt-[0.2rem] text-[50%]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+        <p className="mt-[0.2rem] text-[58%]">
+          Lorem ipsum dolor sit amet cosectetur adipisicing elit. Nam
           consequuntur rem tenetur numquam, reiciendis similique reprehenderit
           quos rerum expedita dolorem.
         </p>
--- BookFlow-React/src/Components/BookRecs.jsx
+++ BookFlow-React/src/Components/BookRecs.jsx
@@ -2,16 +2,16 @@ function BookRec(props) {
   const { image, author, title, new: isNew } = props;
   return (
     <div className="relative recCardContainer" style={{ height: "13rem" }}>
-      <div className="recCards flex w-[100%] justify-center items-center h-[100%] mt-[0rem] relative">
+      <div className="recCards flex w-[100%] items-center h-[100%] mt-[0rem] relative">
         {isNew && (
-          <span className="absolute top-[1rem] right-[13.5rem] -mt-4 -mr-4 rounded-xl bg-[#076527] text-white w-[3rem] h-[1.5rem] flex items-center justify-center text-sm">
+          <span className="absolute top-[1rem] right-[14rem] -mt-1 mr-[0rem] rounded-xl bg-[#076527] text-white w-[3rem] h-[1.2rem] flex items-center justify-center text-sm">
             <h1 className="font-bold">New</h1>
           </span>
         )}
         <img
           src={image}
           alt=""
-          className="w-[25%] h-[80%] shadow-[0.6em_0.7rem_0.7rem_-1px_rgba(0,0,0,0.3)] hover:cursor-pointer"
+          className="w-[6rem] h-[9.5rem] shadow-[0.6em_0.7rem_0.7rem_-1px_rgba(0,0,0,0.3)] hover:cursor-pointer"
         />
         <div className="flex font-bold flex-col ml-[1.1rem] h-[10rem] w-[9rem]">
           <h1 className="text-[#3D3D3D] text-[1rem] mt-[1rem]">{title}</h1>
