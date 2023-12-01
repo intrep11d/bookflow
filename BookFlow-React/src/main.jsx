72232dcd3ea3ec21d7f5115e9600b348d112e09d
Sky Sayson <Skysayson74@gmail.com>
2023-12-01 23:37:55 +0800

    Added necessary routes for Admin-userTable

--- BookFlow-React/src/main.jsx
+++ BookFlow-React/src/main.jsx
@@ -8,6 +8,9 @@ import passwordField from './Pages/Sign-up/UserField-pw.jsx'
 import signUpUser from './Pages/Sign-up/UserFields.jsx'
 import userHomePage from './Pages/User-instance/User-Landing.jsx'
 import UserHome from './Pages/User-instance/User-Home.jsx'
+import UserCategory from './Pages/User-instance/User-Category.jsx'
+import UserAdminTable from './Pages/Admin-instance/Admin-Usertable.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div>
@@ -17,6 +20,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path='/BookFlow-Password' component={passwordField}></Route>
       <Route path='/BookFlow-userInstance' component={userHomePage}></Route>
       <Route path='/BookFlow-userHome' component={UserHome}></Route>
+      <Route path='/BookFlow-userCategory' component={UserCategory}></Route>
+      <Route path='/BookFlow-Admin-userTable' component={UserAdminTable}></Route>
     </Router>
   </div>
 )
