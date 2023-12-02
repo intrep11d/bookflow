15d83b0e3b9618771bac7bb966d998b7b34c08ff
Sky Sayson <Skysayson74@gmail.com>
2023-12-03 03:50:54 +0800

    Routing additions

--- BookFlow-React/src/main.jsx
+++ BookFlow-React/src/main.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import LogIn from './Pages/Log-in.jsx'
-import passwordField from './Pages/Sign-up/UserField-pw.jsx'
-import signUpUser from './Pages/Sign-up/UserFields.jsx'
-import userHomePage from './Pages/User-instance/User-Landing.jsx'
-import UserHome from './Pages/User-instance/User-Home.jsx'
-import UserCategory from './Pages/User-instance/User-Category.jsx'
-import UserAdminTable from './Pages/Admin-instance/Admin-Usertable.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LogIn from "./Pages/Log-in.jsx";
+import passwordField from "./Pages/Sign-up/UserField-pw.jsx";
+import signUpUser from "./Pages/Sign-up/UserFields.jsx";
+import userHomePage from "./Pages/User-instance/User-Landing.jsx";
+import UserHome from "./Pages/User-instance/User-Home.jsx";
+import UserCategory from "./Pages/User-instance/User-Category.jsx";
+import UserAdminTable from "./Pages/Admin-instance/Admin-Usertable.jsx";
+import AdminEntryProfile from "./Components/Admin-Entry-Profile.jsx";
+import BookCard from "./Components/BookCard.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
     <Router>
-      <Route path='/' component={LogIn} exact></Route>
-      <Route path='/BookFlow-Signup' component={signUpUser}></Route>
-      <Route path='/BookFlow-Password' component={passwordField}></Route>
-      <Route path='/BookFlow-userInstance' component={userHomePage}></Route>
-      <Route path='/BookFlow-userHome' component={UserHome}></Route>
-      <Route path='/BookFlow-userCategory' component={UserCategory}></Route>
-      <Route path='/BookFlow-Admin-userTable' component={UserAdminTable}></Route>
+      <Route path="/" component={LogIn} exact></Route>
+      <Route path="/BookFlow-Signup" component={signUpUser}></Route>
+      <Route path="/BookFlow-Password" component={passwordField}></Route>
+      <Route path="/BookFlow-userInstance" component={userHomePage}></Route>
+      <Route path="/BookFlow-userHome" component={UserHome}></Route>
+      <Route path="/BookFlow-userCategory" component={UserCategory}></Route>
+      <Route path="/BookFlow-bookCard" component={BookCard}></Route>
+      <Route
+        path="/BookFlow-Admin-userTable"
+        component={UserAdminTable}
+      ></Route>
+      <Route
+        path="/BookFlow-Admin-entryProfile-:userID"
+        component={AdminEntryProfile}
+      ></Route>
     </Router>
   </div>
-)
+);
